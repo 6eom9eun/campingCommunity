71ceafced33aa8df7e32b748ae9c0d371f9eb4ac
beomgeun <ggoon98@gmail.com>
2024-01-22 18:07:57 +0900

    연동

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'react-project/build')));
+app.use(express.static(path.join(__dirname, 'fe/build')));
 
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
@@ -11,3 +11,7 @@ app.listen(8080, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/fe/build/index.html'));
 }) 
+
+app.get('*', function (요청, 응답) {
+    응답.sendFile(path.join(__dirname, '/fe/build/index.html'));
+  });
