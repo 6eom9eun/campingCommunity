a1ac20c5a5f22a811c7cb09365c56164301a50f1
beomgeun <ggoon98@gmail.com>
2024-01-22 22:46:36 +0900

    server

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/fe/build/index.html'));
 }) 
 
-app.get('*', function (요청, 응답) {
-    응답.sendFile(path.join(__dirname, '/fe/build/index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/fe/build/index.html'));
 });
